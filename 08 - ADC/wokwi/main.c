3648db823ab00929dadad6117374afa6a3bf0347
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-05-06 23:34:27 -0300

    refazendo a atividade 8 de acordo com a proposta

--- 08 - ADC/wokwi/main.c
+++ 08 - ADC/wokwi/main.c
@@ -3,40 +3,75 @@
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "esp_adc/adc_oneshot.h"
+#include "esp_timer.h"
 #include <stdio.h>
-
 
 #define POT_PIN GPIO_NUM_11
 #define PUSH_BUTTON_PIN GPIO_NUM_12
 #define LED_PIN GPIO_NUM_13
 #define ADC_CHANNEL ADC_CHANNEL_0
 
+#define DEBOUNCE_MS 80
+#define PRINT_DELAY 500
+#define ADC_DELAY 15
+
 static adc_oneshot_unit_handle_t adc2_handle;
+static esp_timer_handle_t print_timer, debounce_timer = NULL;
 
+static bool button_state = true;
+static int adc_raw = 0;
+static float tensao = 0;
 
-void timer_uart_send_callback(void* arg){
-}
+void timer_print_callback(void* arg);
+void debounce_timer_callback(void* arg);
+
+void gpio_conf();
+void adc_conf();
+void pwm_conf();
+void timer_conf();
+void isr_conf();
+
+void gpio_interrupt_handler(void* arg);
 
 void app_main() {
+    
+    gpio_conf();
+    adc_conf();
+    pwm_conf();
+    timer_conf();
+    isr_conf();
 
+    while(1){
+        adc_oneshot_read(adc2_handle, ADC_CHANNEL, &adc_raw);
+        tensao = adc_raw*3.3/4095;
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, adc_raw);
+        if(button_state) ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        vTaskDelay(pdMS_TO_TICKS(ADC_DELAY));
+    }
+}
+
+void gpio_conf(){
     gpio_config_t gpio_led = {
         .mode=GPIO_MODE_OUTPUT,
         .pull_up_en=GPIO_PULLUP_DISABLE,
         .pull_down_en=GPIO_PULLDOWN_DISABLE,
-        .pin_bit_mask =(1ULL << LED_PIN) // Pino GPIO15
+        .pin_bit_mask =(1ULL << LED_PIN) // Pino GPIO13
         };
 
     gpio_config(&gpio_led);
 
     gpio_config_t gpio_button = {
         .mode=GPIO_MODE_INPUT,
-        .pull_up_en=GPIO_PULLUP_ENABLE,
+        .pull_up_en=GPIO_PULLUP_DISABLE,
         .pull_down_en=GPIO_PULLDOWN_DISABLE,
-        .pin_bit_mask =(1ULL << PUSH_BUTTON_PIN) // Pino GPIO15
+        .intr_type=GPIO_INTR_NEGEDGE,
+        .pin_bit_mask =(1ULL << PUSH_BUTTON_PIN) // Pino GPIO12
         };
 
     gpio_config(&gpio_button);
+}
 
+void adc_conf(){
     // Configuração do ADC
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_2,
@@ -48,9 +83,10 @@ void app_main() {
         .bitwidth = ADC_BITWIDTH_12,
     };
     adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL, &config);
-    
+}
 
-    ledc_timer_config_t ledc_timer_leds = {
+void pwm_conf(){
+        ledc_timer_config_t ledc_timer_leds = {
         .duty_resolution = LEDC_TIMER_12_BIT,  
         .freq_hz = 1000,                   
         .speed_mode = LEDC_LOW_SPEED_MODE,
@@ -60,24 +96,68 @@ void app_main() {
 
     ledc_timer_config(&ledc_timer_leds);
 
-        ledc_channel_config_t ledc_channel_leds = {
-            .channel = LEDC_CHANNEL_0,
-            .duty = 0,
-            .gpio_num = LED_PIN,                
-            .intr_type = LEDC_INTR_DISABLE,
-            .speed_mode = LEDC_LOW_SPEED_MODE,
-            .timer_sel = LEDC_TIMER_0
-        };
-        
-        ledc_channel_config(&ledc_channel_leds);
+    ledc_channel_config_t ledc_channel_leds = {
+        .channel = LEDC_CHANNEL_0,
+        .duty = 0,
+        .gpio_num = LED_PIN,                
+        .intr_type = LEDC_INTR_DISABLE,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_sel = LEDC_TIMER_0
+    };
+    
+    ledc_channel_config(&ledc_channel_leds);
+}
 
+void timer_conf(){
+    esp_timer_create_args_t timer_args_print = {
+        .callback = timer_print_callback,
+        .arg = NULL,
+        .name = "timer_uart_send_periodic"
+    };
 
-    while(1){
-        int adc_raw;
-        adc_oneshot_read(adc2_handle, ADC_CHANNEL, &adc_raw);
-        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, adc_raw);
-        if(gpio_get_level(PUSH_BUTTON_PIN)) ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-        printf("adc: %d\n", adc_raw);
-        vTaskDelay(pdMS_TO_TICKS(15));
+    esp_timer_create_args_t timer_args_db = {
+        .callback = debounce_timer_callback,
+        .arg = NULL,
+        .name = "debounce_timer"
+    };
+    esp_timer_create(&timer_args_db, &debounce_timer);
+    esp_timer_create(&timer_args_print, &print_timer);
+
+    esp_timer_start_periodic(print_timer, 500000);
+}
+
+void isr_conf(){
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(PUSH_BUTTON_PIN, gpio_interrupt_handler, (void*)PUSH_BUTTON_PIN);
+}
+
+void timer_print_callback(void* arg){
+    printf("\nValor: %d Tensão: %f", adc_raw, tensao);
+    if(button_state){
+        printf(" Estado: LIVE");
+    } else {
+        printf(" Estado: HOLD");
     }
+}
+
+void debounce_timer_callback(void* arg){
+    if(!gpio_get_level(PUSH_BUTTON_PIN)){ // Botão ativo em nivel lógico baixo
+        if(button_state){
+            button_state = false;
+        } else {
+            button_state = true;
+        }
+    }
+    // Reabilita interrupt
+    gpio_intr_enable(PUSH_BUTTON_PIN);
+}
+
+void IRAM_ATTR gpio_interrupt_handler(void* arg) {
+    uint32_t gpio_num = (uint32_t)arg;
+    
+    // Desabilita interrupt durante debounce
+    gpio_intr_disable(gpio_num);
+    
+    // Inicia timer para debounce
+    esp_timer_start_once(debounce_timer, DEBOUNCE_MS*1000);
 }
